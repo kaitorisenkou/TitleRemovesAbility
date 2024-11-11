bb876a1195db1223219a6b578ddb4a0d43081bda
kaitorisenkou <kaitorisenkou@icloud.com>
2024-11-11 16:58:22 +0900

    プロジェクト ファイルを追加します。

---
+++ TitleRemovesAbility/TitleRemovesAbility.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace TitleRemovesAbility {
+    [StaticConstructorOnStartup]
+    public class TitleRemovesAbility {
+        static TitleRemovesAbility() {
+            Log.Message("[TitleRemovesAbility] Now active");
+            var harmony = new Harmony("kaitorisenkou.BoomerangAbility");
+            harmony.Patch(
+                AccessTools.Method(typeof(Pawn_RoyaltyTracker), "OnPostTitleChanged", null, null),
+                    null,
+                    new HarmonyMethod(typeof(TitleRemovesAbility), nameof(Patch_OnPostTitleChanged), null),
+                    null,
+                    null
+                    );
+            Log.Message("[TitleRemovesAbility] Harmony patch complete!");
+
+        }
+        public static void Patch_OnPostTitleChanged(RoyalTitleDef newTitle, ref Pawn ___pawn) {
+            var modEx = newTitle.GetModExtension<ModExtension_TitleRemovesAbility>();
+            if (modEx == null) return;
+            ___pawn.abilities.RemoveAbility(modEx.abilityDef);
+        }
+    }
+
+    public class ModExtension_TitleRemovesAbility : DefModExtension {
+        public AbilityDef abilityDef = null;
+    }
+}
